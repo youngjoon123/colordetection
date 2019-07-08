35ce36378bdf7c0cd33601359b2ebe0abc6caf49
youngjoon <junjun0905@naver.com>
2019-07-08 12:55:10 +0900

    first

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import cv2
 import numpy as np
 import math
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
+import time
+import requests
+url='http://thecolorapi.com/id'
+
+count=0
+#from sklearn.cluster import KMeans
+#import matplotlib.pyplot as plt
+from scipy.spatial import distance
 cap = cv2.VideoCapture(0)
 
 while(1):
-
     try:  #an error comes if it does not find anything in window as it cannot find contour of max area
           #therefore this try error statement
+        # startTime = time.time()
+        # st=time.time()-startTime
 
         ret, frame = cap.read()
         frame=cv2.flip(frame,1)
         #cv2.dilate에서 사용 할 커널의 크기 - 1값이 되는 외곽의 픽셀크기를 설정
-        kernel = np.ones((3,3),np.uint8) 
+        kernel = np.ones((3,3),np.uint8)
 
         #손인식을 할 범위의 사이즈 (100,100),(400,500) 사각형의 모서리
-        roi=frame[100:400, 100:500]
+        roi=frame[100:400, 100:400]
 
-        #roi 사각형의 프레임을 그려준다.
-        cv2.rectangle(frame,(100,100),(500,400),(0,255,0),0)
+        #roi 사각형의 프레임을 그려준다.oy
+        cv2.rectangle(frame,(100,100),(400,400),(0,255,0),0)
 
         #roi 범위 안의 색영역추출
         hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
@@ -81,7 +88,6 @@ while(1):
             far = tuple(approx[f][0])
             pt= (100,180)
 
-            topmost = tuple(cnt[cnt[:, :, 1].argmin()][0])
 
             # end,for,start 점의 삼각형 길이
             a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
@@ -104,25 +110,63 @@ while(1):
                 cv2.circle(roi, end, 6, [255,0,0], 1)
                 cv2.circle(roi, start, 6, [0,0,255], 1)
 
-            
+        
             
             #cv2.circle(roi, (topmost[0],topmost[1]), 6, [255,0,0], 1)
 
             # roi=frame[100:400, 100:500]    &&&&&&&&&&&
-            colorpoint=frame[topmost[0]-5:topmost[0]+5, topmost[1]-5:topmost[1]+5]
-            cv2.rectangle(roi,(topmost[0]-5,topmost[1]-5),(topmost[0]+5,topmost[1]+5),(0,255,0),0)
+            # colorpoint=frame[topmost[0]-5:topmost[0]+5, topmost[1]-5:topmost[1]+5]
+            
+
             #컨벡스홀 라인그리기 start-end로 각각 
             cv2.line(roi,start, end, [0,255,0], 2)
             # lower_top = np.array([0,105,40], dtype=np.uint8)
             # upper_top = np.array([20,255,220], dtype=np.uint8)   &&&&&&&&&&&
+            
+            #print(roi.item(topmost[0],topmost[1],0))
 
-            #mask = cv2.inRange(hsvtop, lower_skin, upper_skin)    &&&&&&&&&&&
+        topmost = tuple(cnt[cnt[:, :, 1].argmin()][0])
+        cv2.rectangle(roi,(topmost[0]-5,topmost[1]-7),(topmost[0]+5,topmost[1]-2),(0,255,0),0)
+        #cv2.circle(roi, (topmost[0],topmost[1]), 4, [0,100,100], -1)
+        #mask = cv2.inRange(hsvtop, lower_skin, upper_skin)    &&&&&&&&&&&
 
         cx = int(M['m10'] / M['m00'])
         cy = int(M['m01'] / M['m00'])
-        cv2.circle(roi, (cx,cy), 6, [255,0,0], 1)
-        l+=1
+        cv2.circle(roi, (cx,cy), 6, [255,0,0], 1) #중점인데 필요없음
+        roi2=frame[100:900, 100:900]
+        rgbroi = cv2.cvtColor(roi2, cv2.COLOR_BGR2RGB)
+        #rgbroi=frame[100:400, 100:500]
+        px=rgbroi[topmost[0]-7,topmost[1]]
+        cv2.circle(roi, (topmost[0],topmost[1]-15), 4, [0,100,100], -1)
+        #리얼타임도중 프레임전달 10프레임당 하나?+ 손가락 갯수가 하나일 때만?
+        print('rgb: ',px)
 
+        # text_rgb = ','.join(map(str, px))
+        # send_data = {'rgb' : text_rgb, 'format' : 'json'}
+        # r=requests.get(url=url,params=send_data)
+
+        # print('rgb의 r',px[0])
+        # test_json = r.json()
+        # print(test_json['name']['value'])
+
+        i=0
+        arr1=np.array([[255,000,000],[000,255,000],[000,000,255],[102,102,102],[000,000,000],[255,255,255],[241,240,209]])
+        #빨초파회검흰베
+
+################### 색 출력부분
+        # for i in len(arr1):
+        #     dists = distance.euclidean(arr1[i,:],px)
+        # #     dists=sum((arr1[i,:]-px)**(1/2))
+        #     temp=dists
+
+        #     if temp>dists:
+        #         temp=dists #없어도 됨
+        #         index=i
+
+        # print(index)  #index 번호에 맞게 색이름 출력하게 수정
+
+
+        l+=1
 
         #display corresponding gestures which are in their ranges
         font = cv2.FONT_HERSHEY_SIMPLEX
@@ -135,6 +179,12 @@ while(1):
                 
                 else:   
                         cv2.putText(frame,'1',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
+                        if count%20==0:
+                            text_rgb = ','.join(map(str, px))
+                            send_data = {'rgb' : text_rgb, 'format' : 'json'}
+                            r=requests.get(url=url,params=send_data)
+                            test_json = r.json()
+                            print(test_json['name']['value'])
 
         elif l==2:
             cv2.putText(frame,'2',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
@@ -160,10 +210,14 @@ while(1):
 
         cv2.imshow('mask',mask)
         cv2.imshow('frame',frame)
+
+        count=count+1
+
         
     except:
         pass
 
+    
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
